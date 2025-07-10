9e0642b41437e3888dc6e3433d4cee7bf9922081
Gabriel Jeronimo <gabrieljvcs@gmail.com>
2025-07-10 18:30:21 -0300

    final update

--- script.js
+++ script.js
@@ -68,18 +68,131 @@ function updateCartModal(){
 
     cart.forEach(item => {
     const cartItemElement = document.createElement("div")
-    cartItemElement.innerHTML = `
-        <div>
-            <p>${item.name}</p>
-            <p>${item.quantity}</p>
-            <p>R$${item.price}</p>    
-        </div>
+    cartItemElement.classList.add("flex", "justify-between", "mb-4", "flex-col")
 
-        <div> 
-            <button> Remover </button>
+    cartItemElement.innerHTML = `
+        <div class="flex items-center justify-between">
+            <div>
+                <p class="font-medium">${item.name}</p>
+                <p>Quantidade: ${item.quantity}</p>
+                <p class="font-medium mt-2">R$${item.price.toFixed(2)}</p>    
+            </div>
+ 
+            <button class="remove-from-cart-btn hover:bg-red-600 duration-200 hover:text-white px-4 py-1 rounded" data-name="${item.name}">
+            Remover 
+            </button>
+        
         </div>
     `   
     
+    total += item.price * item.quantity
+    
     cartItemsContainer.appendChild(cartItemElement)
     })
+
+    cartTotal.textContent = total.toLocaleString("pt-BR", {
+        style: "currency",
+        currency: "BRL"
+    })
+
+    cartCounter.innerHTML = cart.length;
+
+}
+
+// Função para remover item do carrinho
+cartItemsContainer.addEventListener("click", function(event){
+    if(event.target.classList.contains("remove-from-cart-btn")){
+        const name = event.target.getAttribute("data-name")
+    
+        removeItemCart(name);
+    }
+})
+
+function removeItemCart(name){
+    const index = cart.findIndex(item => item.name === name);
+
+    if(index !== -1){
+        const item = cart[index];
+        
+        if(item.quantity > 1){
+            item.quantity -= 1;
+            updateCartModal();
+            return;
+        }
+
+        cart.splice(index, 1);
+        updateCartModal();
+    }
+}
+
+//Função para pegar o que você escreve no input de endereço
+addressInput.addEventListener("input", function(event){
+    let inputValue = event.target.value;
+
+    if(inputValue !== ""){
+        addressInput.classList.remove("border-red-600")
+        addressWarn.classList.add("hidden")
+    }
+})
+
+//Finalizar pedido
+checkoutBtn.addEventListener("click", function(){
+
+    const isOpen = isRestaurantOpen();
+    if(!isOpen){
+        Toastify({
+            text: "Ops, restaurante fechado no momento!",
+            duration: 3000,
+            close: true,
+            gravity: "top", // `top` or `bottom`
+            position: "center", // `left`, `center` or `right`
+            stopOnFocus: true, // Prevents dismissing of toast on hover
+            style: {
+                background: "#ef4444",
+            },
+        }).showToast();
+        return;
+    }
+
+    if(cart.length === 0) return;
+
+    if(addressInput.value === ""){
+        addressWarn.classList.remove("hidden")
+        addressInput.classList.add("border-red-600")
+        return;
+    }
+
+    // Enviar pedido para o WhatsApp
+    const cartItems = cart.map((item) => {
+        return (
+            `${item.name}, Quantidade: (${item.quantity}), Preço: R$${item.price}\n`
+        )
+    }).join("")
+
+    const message = encodeURIComponent(cartItems)
+    const phone = "+5581986213573"
+
+    window.open(`https://wa.me/${phone}?text=${message}Endereço: ${addressInput.value}`, "_blank")
+
+    cart = [];
+    updateCartModal();
+
+})
+
+//Verificar se o restaurante está aberto de acordo com a hora
+function isRestaurantOpen(){
+    const data = new Date();
+    const hora = data.getHours();
+    return hora >= 18 && hora < 22;
+}
+
+const spanItem = document.getElementById("date-span")
+const isOpen = isRestaurantOpen();
+
+if(isOpen){
+    spanItem.classList.remove("bg-red-500")
+    spanItem.classList.add("bg-green-600")
+} else {
+    spanItem.classList.remove("bg-green-600")
+    spanItem.classList.add("bg-red-600")
 }
